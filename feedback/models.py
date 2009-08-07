4d5f6503e8635039660f5e001ce40198f892ee2f
makestory <makestory@zoho.com>
2009-08-07 15:51:01 +0800

    fix Message model bug

--- feedback/models.py
+++ feedback/models.py
@@ -19,16 +19,17 @@ class Message(models.Model):
     content = models.TextField('内容')
     date = models.DateTimeField('日期',auto_now_add=True)
     ip = models.CharField('IP地址',max_length=20)
+    reply=models.TextField('回复',blank=True)
     show = models.BooleanField('显示',default=True)
     at = models.ForeignKey(Daily,blank=True,null=True,verbose_name ='关联')
     
     def save(self):
-        super(Message,self).save(self)
+        models.Model.save(self)
         if self.at:
             self.at.count=Message.objects.filter(at=self.at).count()
             self.at.save()
     def delete(self):
-        super(Message,self).save(self)
+        models.Model.delete(self)
         if self.at:
             self.at.count=Message.objects.filter(at=self.at).count()
             self.at.save()
