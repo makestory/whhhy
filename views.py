f9d6af9fe679f5bd5a1a4aa455eee1569f3cfe12
makestory <makestory@zoho.com>
2009-08-31 00:11:52 +0800

    fix bugs and add myjob

--- views.py
+++ views.py
@@ -7,15 +7,17 @@ from daily.models import Daily
 class HomeView(BaseView):
     @classmethod
     def homepage(cls,request):
-        para=BaseView().para
+        para = BaseView().para
         return render_to_response('base.html',para)
     
     @classmethod
     def about(cls,request):
-        para=BaseView().para
+        para = BaseView().para
         return render_to_response('about.html',para)
     
 
 def rss(request):
-    dailys=Daily.objects.filter(show=True).all()[:10]
-    return render_to_response('rss.xml',{'dailys':dailys})
+    dailys = Daily.objects.filter(show=True).all()[:10]
+    response = render_to_response('rss.xml',{'dailys':dailys})
+    response._headers =  {'content-type': ('Content-Type', 'text/xml; charset=UTF-8')}
+    return response
